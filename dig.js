3fa33fc641626067256f8c10cc6ee11d108cbd46
andreweverman <everman.32@osu.edu>
2019-12-31 13:57:30 -0500

    tokens can refresh, can add songs to dig

--- dig.js
+++ dig.js
@@ -4,8 +4,6 @@ const config = require('./bin/config.json');
 var models = require('./dig_db')(mongoose);
 
 
-
-
 // from the scheduler
 // will go and look in the mongodb for all users who will want to have dig run for them
 // pass these users on to the dig function below
@@ -18,25 +16,28 @@ function run_dig() {
             models.User.findOne({ user_id: member.user_id }).exec(function (err, user) {
                 if (err) throw err;
 
-                user_dig = new Dig(member.user_id, member.dig_id, member.dug_id, member.last_run, user.access_token);
-                user_dig.dig();
+                new Dig(member.user_id, member.dig_id, member.dug_id, member.last_run, user.access_token, user, digs);
+
             });
         });
 
-        console.log("Done")
+        console.log("Done Digging")
 
     });
 
 }
 
 class Dig {
-    constructor(user_id, dig_id, dug_id, last_run, access_token) {
+    constructor(user_id, dig_id, dug_id, last_run, access_token, user_db, dig_db) {
         // setting needed variables
         this.user_id = user_id;
         this.dig_id = dig_id;
         this.dug_id = dug_id;
         this.access_token = access_token;
         this.last_run = last_run
+
+        this.user_db = user_db;
+        this.dig_db = dig_db;
 
 
         // setting access_token
@@ -55,9 +56,6 @@ class Dig {
 
         // check if there is something new that would mean we should add to dig
 
-
-      
-
         if (this.check_run()) {
             // TODO: add to dig
             this.add_dig();
@@ -67,35 +65,76 @@ class Dig {
             // TODO: sort dig by album. make optional user parameter
 
             // TODO:  write that we ran dig in user
-
-
         }
 
 
         // TODO: trim tracks from dig
 
-
-
-
-
-
-
     }
 
     check_run() {
-        
-        // return this.last_run < this.dig_tracks[0]
+        return this.last_run < new Date(this.saved_tracks[0].added_at);
+    }
+
+
+    diff(track_added) {
+        const oneDay = 24 * 60 * 60 * 1000;
+
+        let current_time = new Date();
+
+        return Math.round(Math.abs((current_time - track_added) / oneDay));
 
     }
 
 
     add_dig() {
 
-       
+        let new_saved_tracks = this.new_saved_tracks();
+        var dig = this;
+
+
+        this.spotify_api.addTracksToPlaylist(this.dig_id, new_saved_tracks, { position: 0 }).then(function (data) {
+
+            models.Dig.findOne({ user_id: dig.user_id }).exec(function (err, user) {
+                if (err) throw err;
+
+                user.last_run = new Date();
+                user.save(err, user => {
+                    if (err) return console.error(err);
+                });
+
+            });
+
+        }, function (err) {
+            console.log('Something went wrong!', err);
+        });
+
+
     }
 
-    async get_necessary() {
-        await Promise.all([
+    new_saved_tracks() {
+
+        let stop = this.saved_tracks.length;
+        let new_tracks = []
+
+        for (let i = 0; i < this.saved_tracks.length; i++) {
+            let track = this.saved_tracks[i]
+
+            if (this.last_run > new Date(track.added_at)) {
+
+                break;
+            }
+            else {
+                new_tracks.push(track.track.uri)
+            }
+        }
+
+        return new_tracks;
+    }
+
+    get_necessary() {
+
+        Promise.all([
             this.spotify_api.getMySavedTracks({
                 limit: 50,
                 offset: 0
@@ -108,13 +147,14 @@ class Dig {
         ]).then(result => {
 
             this.saved_tracks = result[0].body.items;
-            this.saved_albums = result[1];
-            this.dig_tracks = result[2];
+            this.saved_albums = result[1].body.items;
+            this.dig_tracks = result[2].body.items;
 
+            this.dig();
+        }).catch(error => {
+            console.error("Dig: ", error)
         });
     }
-
-
 }
 
 
---
+++ refresh.js
@@ -0,0 +1,71 @@
+var spotify_web_api = require('spotify-web-api-node');
+var mongoose = require('mongoose')
+const config = require('./bin/config.json');
+var models = require('./dig_db')(mongoose);
+var request = require('request');
+
+
+// will go and look in the mongodb for all users who will want to have dig run for them
+// pass these users on to the dig function below
+function refresh_tokens() {
+
+
+
+
+    models.User.find().exec(function (err, users) {
+        if (err) throw err;
+        users.forEach(user => {
+            user_dig = new Refresh(user)
+        });
+    });
+
+    console.log("Done Refreshing Tokens");
+}
+
+
+class Refresh {
+    constructor(user) {
+        this.refresh_token = user.refresh_token;
+        this.user_id = user.user_id;
+
+        this.refresh()
+
+    }
+
+
+    refresh() {
+        var user = this;
+        let refresh_token = this.refresh_token
+        let client_id = config.clientID;
+        let client_secret = config.clientSecret;
+
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            headers: { 'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')) },
+            form: {
+                grant_type: 'refresh_token',
+                refresh_token: refresh_token
+            },
+            json: true
+        };
+
+        request.post(authOptions, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var access_token = body.access_token;
+
+                // need to save the updated access_token in the mongo_db
+                models.User.findOrCreate({ user_id: user.user_id }, function (err, user) {
+                    user.access_token = access_token;
+
+                    // saving user changes
+                    user.save(err, user => {
+                        if (err) return console.error(err);
+                    });
+
+                });
+            }
+        });
+    }
+}
+
+module.exports = refresh_tokens;
