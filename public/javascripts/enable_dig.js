a90527714c3cad508e06121ca84a909a532eacf9
andreweverman <everman.32@osu.edu>
2020-01-02 04:01:54 -0500

    some start on dug

--- public/javascripts/enable_dig.js
+++ public/javascripts/enable_dig.js
@@ -5,11 +5,10 @@ document.getElementById("submit_dig").addEventListener("click", validate);
 function validate() {
 
     let dig_select = document.getElementById("dig_select").value;
-    let dug_select = document.getElementById("dug_select").value;
 
     if (dig_select != "" && dig_select != dug_select) {
         document.write("Redirecting");
-        location.assign('/enable_dig/valid?dig_id=' + dig_select+"&dug_id="+dug_select);
+        location.assign('/enable_dig/valid?dig_id=' + dig_select);
 
     }
 }
