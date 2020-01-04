f81da26f90e05a1b999e9ea42d3b88c21119918f
andreweverman <everman.32@osu.edu>
2020-01-04 01:01:29 -0500

    disable buttons works

--- public/javascripts/enable_dig.js
+++ public/javascripts/enable_dig.js
@@ -1,16 +1,26 @@
-document.getElementById("submit_dig").addEventListener("click", validate);
 
 
+// submitting dig. extra check for if there is something selected
+$('#submit_dig').click(
+    function validate() {
 
-function validate() {
-
-    let dig_select = document.getElementById("dig_select").value;
-
-    if (dig_select != "") {
-        document.write("Redirecting");
-        location.assign('/enable_dig/valid?dig_id=' + dig_select);
-
+        let dig_select = document.getElementById("dig_select").value;
+    
+        if (dig_select != "") {
+            document.write("Redirecting");
+            location.assign('/enable_dig/valid?dig_id=' + dig_select);
+    
+        }
     }
-}
+
+)
+
+
+// for the disabling of the button
+$('#dig_credentials').change(function () {
+    if ($('#dig_select').val() != ''){
+        $('#submit_dig').prop('disabled', false)
+    }
+})
 
 
--- public/javascripts/enable_dug.js
+++ public/javascripts/enable_dug.js
@@ -1,16 +1,26 @@
-document.getElementById("submit_dug").addEventListener("click", validate);
 
 
+// submitting dig. extra check for if there is something selected
+$('#submit_dug').click(
+    function validate() {
 
-function validate() {
-
-    let dug_select = document.getElementById("dug_select").value;
-
-    if (dug_select != "") {
-        document.write("Redirecting");
-        location.assign('/enable_dug/valid?dug_id=' + dug_select);
-
+        let dug_select = document.getElementById("dug_select").value;
+    
+        if (dug_select != "") {
+            document.write("Redirecting");
+            location.assign('/enable_dug/valid?dug_id=' + dug_select);
+    
+        }
     }
-}
+
+)
+
+
+// for the disabling of the button
+$('#dug_credentials').change(function () {
+    if ($('#dug_select').val() != ''){
+        $('#submit_dug').prop('disabled', false)
+    }
+})
 
 
