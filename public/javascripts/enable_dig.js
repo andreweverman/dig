39cd869d69ce1db18ca779b51d0df1b5ab7435ef
andreweverman <everman.32@osu.edu>
2020-01-05 00:43:37 -0500

    redid the look for enable dig a bit. still need to make create playlist option

--- public/javascripts/enable_dig.js
+++ public/javascripts/enable_dig.js
@@ -2,25 +2,42 @@
 
 // submitting dig. extra check for if there is something selected
 $('#submit_dig').click(
-    function validate() {
+    function submit() {
 
-        let dig_select = document.getElementById("dig_select").value;
-    
-        if (dig_select != "") {
+        let val = $(".existing_playlists").find('.list-group-item-success')
+        let dig_id = val[0].value
+        if (dig_id != "") {
             document.write("Redirecting");
-            location.assign('/enable_dig/valid?dig_id=' + dig_select);
-    
+            location.assign('/enable_dig/valid?dig_id=' + dig_id);
+
         }
     }
 
 )
 
 
-// for the disabling of the button
-$('#dig_credentials').change(function () {
-    if ($('#dig_select').val() != ''){
+// this is for the selecting of the playlist element
+$('.playlist_item').click(function change() {
+
+    let find_primary = $(this.parentElement).find('.list-group-item-success');
+    if (find_primary.length != 0) {
+        for (let i = 0; i < find_primary.length; i++) {
+            $(find_primary[i]).removeClass("list-group-item-succecss")
+            $(find_primary[i]).addClass("list-group-item-dark")
+        }
+
+    }
+
+    if ($(this).hasClass("list-group-item-dark")) {
+        $(this).removeClass("list-group-item-dark")
+        $(this).addClass("list-group-item-success")
+    }
+
+    if ($('#dig_select').val() != '') {
         $('#submit_dig').prop('disabled', false)
     }
 })
+
+
 
 
