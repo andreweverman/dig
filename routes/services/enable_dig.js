fed25217452d3773c10d71b7913beeafe936edfa
andreweverman <everman.32@osu.edu>
2020-01-08 01:39:28 -0500

    create dig is now a put request. eventually need to spread new way around

--- routes/services/enable_dig.js
+++ routes/services/enable_dig.js
@@ -30,44 +30,11 @@ router.get('/', ensureAuthenticated, function (req, res) {
 });
 
 // handles the enabling of dig. only good input can get to this point
-router.post('/', ensureAuthenticated, function (req, res) {
+router.put('/', ensureAuthenticated, function (req, res) {
     let user = req.user;
+    if (user) {
 
-    if (req.body.dig_id) { // set the variables in mongoose for the dig
-        models.Dig.findOrCreate({ user_id: user.user_id }, function (err, dig) {
-
-            // editing new user
-            if (!dig.dig_id) {
-                dig.user_id = user.user_id;
-                // arbitrary date that is too far back intentionally
-                dig.last_run = new Date("1998-07-12T16:00:00Z");
-            }
-
-            dig.dig_id = req.body.dig_id;
-
-            service_util.add_service_to_user(service_name, user.user_id);
-
-            // saving user changes
-            dig.save(err, dig => {
-                if (err) return console.error(err);
-            });
-        });
-
-        res.redirect('/');
-
-    }
-    else if (req.body.new_playlist_name) {
-        // user is creating a playlist
-
-        let dig_playlist_name = req.body.new_playlist_name;
-
-        let spotify_api = new spotify_web_api(config);
-        spotify_api.setAccessToken(req.user.access_token);
-
-        spotify_api.createPlaylist(user.user_id, dig_playlist_name, { 'description': "Playlist created to hold recently saved tracks. ", 'public': false, 'collaborative': false }).then(data => {
-
-            // need to add to dig db
-            console.log(data)
+        if (req.body.dig_id) { // set the variables in mongoose for the dig
             models.Dig.findOrCreate({ user_id: user.user_id }, function (err, dig) {
 
                 // editing new user
@@ -77,20 +44,58 @@ router.post('/', ensureAuthenticated, function (req, res) {
                     dig.last_run = new Date("1998-07-12T16:00:00Z");
                 }
 
-                dig.dig_id = data.body.id;
+                dig.dig_id = req.body.dig_id;
 
                 service_util.add_service_to_user(service_name, user.user_id);
 
                 // saving user changes
                 dig.save(err, dig => {
                     if (err) return console.error(err);
+                 
+                });
+                res.redirect(200, '/');
+            });
+
+       
+
+        }
+        else if (req.body.new_playlist_name) {
+            // user is creating a playlist
+
+            let dig_playlist_name = req.body.new_playlist_name;
+
+            let spotify_api = new spotify_web_api(config);
+            spotify_api.setAccessToken(req.user.access_token);
+
+            spotify_api.createPlaylist(user.user_id, dig_playlist_name, { 'description': "Playlist created to hold recently saved tracks. ", 'public': false, 'collaborative': false }).then(data => {
+
+                // need to add to dig db
+                console.log(data)
+                models.Dig.findOrCreate({ user_id: user.user_id }, function (err, dig) {
+
+                    // editing new user
+                    if (!dig.dig_id) {
+                        dig.user_id = user.user_id;
+                        // arbitrary date that is too far back intentionally
+                        dig.last_run = new Date("1998-07-12T16:00:00Z");
+                    }
+
+                    dig.dig_id = data.body.id;
+
+                    service_util.add_service_to_user(service_name, user.user_id);
+
+                    // saving user changes
+                    dig.save(err, dig => {
+                        if (err) return console.error(err);
+                    });
                 });
             });
-        });
 
 
-        res.redirect('/');
+            res.redirect('/');
+        }
     }
+
 
 });
 
