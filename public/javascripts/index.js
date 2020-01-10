9b003d8acdc8be6ac382ad3ab42a313db9ebd7fd
andreweverman <everman.32@osu.edu>
2020-01-10 15:05:02 -0500

    moving to desktop

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,22 +1,19 @@
 $(".disable_service").click(
     function disable() {
+
+        let disable_url = $(this).attr('id');
         if (confirm("Are you sure?")) {
             $.ajax({
-                url: '/disable_dig',
+                url: disable_url,
                 type: 'DELETE',
-
                 success: function (response) {
-                
+
                     window.location.reload(true);
-                }
-                ,
+                },
                 error: function (request, msg, error) {
                     window.location.reload(true);
                 }
             });
-
-
         }
-
     }
 )
