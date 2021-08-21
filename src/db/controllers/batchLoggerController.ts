cf3f4b546c3151336a341ac7f6146487e5787e0b
everman-ss <85192512+everman-ss@users.noreply.github.com>
2021-08-21 01:58:34 -0400

    fix of time

--- src/db/controllers/batchLoggerController.ts
+++ src/db/controllers/batchLoggerController.ts
@@ -4,6 +4,7 @@ import BatchLoggers, { IBatchLogger } from '../models/BatchLogger'
 export namespace BatchLogger {
     export function createLog(serviceName: string): Promise<IBatchLogger> {
         const env = process.env.ENV
-        return BatchLoggers.create({ serviceName, env })
+        const time = new Date()
+        return BatchLoggers.create({ serviceName, env, time })
     }
 }
--- src/db/controllers/loggerController.ts
+++ src/db/controllers/loggerController.ts
@@ -3,6 +3,7 @@ import Loggers, { ILoggerDoc } from '../models/Logger'
 
 export namespace Logger {
     export function createLog(userID: ObjectId, serviceName: string, error: string): Promise<ILoggerDoc> {
-        return Loggers.create({ userID, serviceName, error })
+        const time = new Date()
+        return Loggers.create({ userID, serviceName, error, time })
     }
 }
